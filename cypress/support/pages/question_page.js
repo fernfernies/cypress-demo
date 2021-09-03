7c4ae428f7aa26343a007736ab44e133f077a0a3
atchariya.sae <atchariyas@rabbit.com>
2021-09-03 09:12:21 +0700

    test

---
+++ cypress/support/pages/question_page.js
@@ -0,0 +1,38 @@
+export const question_element = {
+    sel_carbrand:       '//select[@name="carbrand"]',
+    sel_carmodel:       '//select[@name="carmodel"]',
+    sel_carmanufac:     '//select[@name="carmanufac"]',
+    txt_hmborrow:       '//*[@id="price-hmborrow"]',
+    btn_next1:          '//*[@id="hmborrow"]//*[@type="button"]',
+    rdo_debt_free_no:   '//*[@for="debtfree-N"]',
+    rdo_debt_free_yes:  '//*[@for="debtfree-Y"]',
+    rdo_hmrepaid_a1:    '//*[@for="hmrepaid-A1"]',
+    rdo_hmrepaid_a2:    '//*[@for="hmrepaid-A2"]',
+    rdo_hmrepaid_a3:    '//*[@for="hmrepaid-A3"]',
+    rdo_latepayment_a1: '//*[@for="latepayment-A1"]',
+    rdo_latepayment_a2: '//*[@for="latepayment-A2"]',
+    rdo_latepayment_a3: '//*[@for="latepayment-A3"]',
+    rdo_latepayment_a4: '//*[@for="latepayment-A4"]',
+    sel_province:       '//select[@name="province"]',
+    sel_district:       '//select[@name="district"]',
+    sel_zipcode:        '//select[@name="zipcode"]',
+    sel_occupation:     '//select[@name="occupation"]',
+    sel_income:         '//select[@name="income"]',
+    txt_firstname:      '//*[@name="name"]',
+    txt_lastname:       '//*[@name="surname"]',
+    txt_phone:          '//*[@data-testid="phone-input"]',
+    txt_email:          '//*[@data-testid="email-input"]',
+    btn_next2:          '//*[@id="name"]//*[@type="button"]',
+    btn_next3:          '//*[@id="phone"]//*[@type="button"]',
+    rdo_consent:        '//*[@for="tc-1"]',
+    rdo_scb:            '//*[contains(text(),"SCB")]',
+    btn_proceed:        '//button[@id="btn-marketing-consent"]'
+
+};
+
+
+export const question_messages = {
+    lbl_thankyou_msg_en:          '//*[. = "Thank you for completing our online application."]',
+    
+    
+};
